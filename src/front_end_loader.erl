1b8345e34e68cf914b9e60ea74f6d83bcac39b18
madlep <madlep@ubercharged.net>
2011-04-01 00:19:54 +1100

    output just the path, and parsed line count in main test harness

--- src/front_end_loader.erl
+++ src/front_end_loader.erl
@@ -2,16 +2,18 @@
 
 -export([main/0]).
 
+-include("front_end_loader.hrl").
+
 main() ->
   {ok, FileReaderPid} = fel_file_reader:start_link("tmp/example_logs/atlas-repo.prod.lpo.log"),
   read_all_from_file(FileReaderPid).
   
 read_all_from_file(FileReaderPid) ->
-  read_all_from_file(fel_file_reader:get_next_request(FileReaderPid), FileReaderPid).
+  read_all_from_file(fel_file_reader:get_next_request(FileReaderPid), FileReaderPid, 0).
   
-read_all_from_file(eof, _FileReaderPid) ->
-  io:format("~neof. Done!~n"),
+read_all_from_file(eof, _FileReaderPid, LineCount) ->
+  io:format("~neof. Done! ~w lines parsed~n", [LineCount]),
   halt();
-read_all_from_file(Line, FileReaderPid) ->
-  io:format("~p~n", [Line]),
-  read_all_from_file(fel_file_reader:get_next_request(FileReaderPid), FileReaderPid).
+read_all_from_file(Line, FileReaderPid, LineCount) ->
+  io:format("~s~n", [Line#log_line.request#request.path]),
+  read_all_from_file(fel_file_reader:get_next_request(FileReaderPid), FileReaderPid, LineCount + 1).
