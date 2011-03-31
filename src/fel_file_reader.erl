4e6aab8ace0c4d98dcfffc0736c58719f7d53327
madlep <madlep@ubercharged.net>
2011-04-01 00:08:23 +1100

    cleaned up compiled regex handling by putting them into closure funs instead of having the raw patterns kicking around in the state

--- src/fel_file_reader.erl
+++ src/fel_file_reader.erl
@@ -29,26 +29,31 @@ get_next_request(FileReader) ->
 %% gen_server Function Definitions
 %% ------------------------------------------------------------------
 
--record (ffr_state, {regexes, log_file, done=false}).
+-record (ffr_state, {log_file, parsers, done=false}).
+-record (ffr_parsers, {line_parser, time_parser, request_parser}).
 
 init(LogFileName) ->
   {ok, LogFile} = file:open(LogFileName, [raw, read_ahead]),
+  % handle combined log format e.g:
   % 10.61.104.254 - - [23/Mar/2011:00:00:01 +0000] "GET /app_metrics HTTP/1.1" 200 458 "-" "curl/7.15.5 (x86_64-redhat-linux-gnu) libcurl/7.15.5 OpenSSL/0.9.8b zlib/1.2.3 libidn/0.6.5"
-  % handle combined log format
-  LogPattern = "(?<host>.+?)\\s(?<remote_logname>.+?)\\s(?<remote_user>.+?)\\s\\[(?<time>.+?)\\]\\s\"(?<request>.+?)\"\s(?<status>\\d+)\\s(?<response_bytesize>\\d+|-)\\s\"(?<referrer>.+?)\"\\s\"(?<user_agent>.+?)\"",
-  {ok, CompiledLogLineRe} = re:compile(LogPattern),
+  LineParser = make_regex_runner_fun(
+    "(?<host>.+?)\\s(?<remote_logname>.+?)\\s(?<remote_user>.+?)\\s\\[(?<time>.+?)\\]\\s\"(?<request>.+?)\"\s(?<status>\\d+)\\s(?<response_bytesize>\\d+|-)\\s\"(?<referrer>.+?)\"\\s\"(?<user_agent>.+?)\"", 
+    binary),
 
   % 23/Mar/2011:00:00:01 +0000
-  TimePattern = "(\\d+)/(\\w{3})/(\\d{4,}):(\\d{2}):(\\d{2}):(\\d{2}) ([+|-]?\\d{4})",
-  {ok, CompiledTimeRe} = re:compile(TimePattern),
+  TimeParser = make_regex_runner_fun("(\\d+)/(\\w{3})/(\\d{4,}):(\\d{2}):(\\d{2}):(\\d{2}) ([+|-]?\\d{4})", list),
 
   % GET /app_metrics HTTP/1.1
-  RequestPattern = "(\\w+)\\s([^\s]+)(?:\\s(.+))?$",
-  {ok, RequestRe} = re:compile(RequestPattern),
+  RequestParser = make_regex_runner_fun("(\\w+)\\s([^\s]+)(?:\\s(.+))?$", binary),
 
   State = #ffr_state{
-    regexes=[{log_line, CompiledLogLineRe}, {time, CompiledTimeRe}, {request, RequestRe}], 
-    log_file=LogFile},
+    log_file  =LogFile,
+    parsers   = #ffr_parsers{
+                  line_parser = LineParser, 
+                  time_parser = TimeParser, 
+                  request_parser = RequestParser
+                }
+  },
   {ok, State}.
 
 handle_call(get_next_request, _From, State = #ffr_state{done = true}) ->
@@ -59,7 +64,7 @@ handle_call(get_next_request, From, State) ->
   case Result of
     {ok, Line}  ->
       spawn_link(fun() ->
-        gen_server:reply(From, create_log_line(Line, State#ffr_state.regexes))
+        gen_server:reply(From, create_log_line(Line, State#ffr_state.parsers))
       end),
       {noreply, State};
     eof ->
@@ -86,18 +91,20 @@ code_change(_OldVsn, LogFile, _Extra) ->
 %% ------------------------------------------------------------------
 %% Internal Function Definitions
 %% ------------------------------------------------------------------
-create_log_line(eof, _Regexes) ->
+create_log_line(eof, #ffr_parsers{}) ->
   eof;
-create_log_line(Line, Regexes) ->
-  LogRegexes = proplists:get_value(log_line, Regexes),
-  {match, Matches} = re:run(Line, LogRegexes, [{capture, all_but_first, binary}]),
-  [Host, RemoteLogname, RemoteUser, Time, Request, Status, ResponseBytesize, Referrer, UserAgent] = Matches,
+create_log_line(Line, Parsers = #ffr_parsers{}) ->
+  LineParser = Parsers#ffr_parsers.line_parser,
+  TimeParser = Parsers#ffr_parsers.time_parser,
+  RequestParser = Parsers#ffr_parsers.request_parser,
+  
+  [Host, RemoteLogname, RemoteUser, Time, Request, Status, ResponseBytesize, Referrer, UserAgent] = LineParser(Line),
   #log_line{
     host              = blank_to_undefined(Host), 
     remote_logname    = blank_to_undefined(RemoteLogname), 
     remote_user       = blank_to_undefined(RemoteUser), 
-    time              = parse_date_time(Time, proplists:get_value(time, Regexes)), 
-    request           = create_request(Request, proplists:get_value(request, Regexes)), 
+    time              = parse_date_time(TimeParser(Time)), 
+    request           = create_request(RequestParser(Request)), 
     status            = to_i(binary_to_list(Status)), 
     response_bytesize = to_i(binary_to_list(ResponseBytesize)), 
     referrer          = blank_to_undefined(Referrer), 
@@ -109,10 +116,8 @@ blank_to_undefined(<<"-">>) ->
 blank_to_undefined(Anything) ->
   Anything.
   
-parse_date_time(DateTimeStr, Regex) ->
+parse_date_time([DateStr, MonthStr, YearStr, HourStr, MinuteStr, SecondStr, "+0000"]) ->
   % 23/Mar/2011:00:00:01 +0000
-  {match, Matches} = re:run(DateTimeStr, Regex, [{capture, all_but_first, list}]),
-  [DateStr, MonthStr, YearStr, HourStr, MinuteStr, SecondStr, "+0000"] = Matches, %barf if TZ isn't UTC (+0000)
   Date = {to_i(YearStr), to_i(MonthStr), to_i(DateStr)},  
   Time = {to_i(HourStr), to_i(MinuteStr), to_i(SecondStr)},  
   {Date, Time}.
@@ -136,14 +141,23 @@ to_i(S) ->
       N
   end.
   
-create_request(Request, Regex) ->
-  REResult = re:run(Request, Regex, [{capture, all_but_first, binary}]),
-  case REResult of
-    {match, [Method, Path, HttpVersion]} ->
-      #request{method=Method, path=Path, http_version=HttpVersion};
-    {match, [Method, Path]} ->
-      #request{method=Method, path=Path};
-    nomatch ->
-      undefined
+create_request(nomatch) ->
+  nomatch;
+create_request([Method, Path, HttpVersion]) ->
+  #request{method=Method, path=Path, http_version=HttpVersion};
+create_request([Method, Path]) ->
+  #request{method=Method, path=Path}.
+  
+make_regex_runner_fun(Pattern, CaptureType) ->
+  {ok, Regex} = re:compile(Pattern),
+  fun(Str) ->
+    Match = re:run(Str, Regex, [{capture, all_but_first, CaptureType}]),
+    case Match of
+      {match, Matches} ->
+        Matches;
+      nomatch ->
+        nomatch
+    end
   end.
+  
   
